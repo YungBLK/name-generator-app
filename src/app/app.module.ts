176598dd80dc9fb4c0c3b040ab07a27dc99fafaf
Igor Lucio Vieira <lucioigf2@gmail.com>
2022-01-02 13:23:47 -0300

    angular new cli project, component name generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NameGeneratorComponent } from './name-generator/name-generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameGeneratorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/name-generator/name-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-generator',
+  templateUrl: './name-generator.component.html',
+  styleUrls: ['./name-generator.component.css']
+})
+export class NameGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
