5d96a4d42a777d2a26006e00cbc7a9ded86f856e
Igor Lucio Vieira <lucioigf2@gmail.com>
2022-01-10 11:18:18 -0300

    header + name-generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NameGeneratorComponent } from './name-generator/name-generator.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NameGeneratorComponent
+    NameGeneratorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
--- src/app/name-generator/name-generator.component.ts
+++ src/app/name-generator/name-generator.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-name-generator',
   templateUrl: './name-generator.component.html',
   styleUrls: ['./name-generator.component.css']
 })
 export class NameGeneratorComponent implements OnInit {
+  name: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getRandom(min: number, max: number) {
+    return min + Math.floor(Math.random() * (max - min + 1))
+  }
+
+  getOneConsonant(consonants: string) {
+    let start = this.getRandom(1, 20);
+    let end = start + 1;
+    console.log("consonant", start, consonants.slice(start, end))
+    return consonants.slice(start, end);
+  }
+
+  getOneVowel(vowel: string) {
+    let start = this.getRandom(1, 4);
+    let end = start + 1;
+    console.log("vowel", start, vowel.slice(start, end))
+    return vowel.slice(start, end);
+  }
+
+  formOneSyllable(vowels: string, consonants: string) {
+    const syllable =
+      this.getOneConsonant(consonants) + this.getOneVowel(vowels);
+    return syllable;
+  }
+
+  generateNewName() {
+    const vowels = 'AEIOU';
+    const consonants = 'QWRTYPSDFGHJKLMNBVCXZ';
+    const numberOfSyllables = this.getRandom(2, 4);
+    console.log("number Of Syllables", numberOfSyllables)
+    this.name = '';
+
+    for (let i = 1; i <= numberOfSyllables; i++) {
+      this.name += this.formOneSyllable(vowels, consonants).toLowerCase();
+    }
   }
 
 }
